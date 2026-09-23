a81f2181f228754f506e80f9c4c5a9bdace7c7a3
sholahuddin57 <sholahuddin.nurfuady@gmail.com>
2026-09-23 22:14:48 +0700

    refactor: mengoptimalkan logika JS menggunakan function

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 const btnTabungan = document.getElementById('btn-tabungan');
-    btnTabungan.addEventListener('click', function() {
-        event.preventDefault();
-        alert('Terimakasih! Pengajuan Tabungan Safari Anda sedang diproses dan akan segera dikonfirmasi oleh tim kami.');
-    });
-    
-
-/*
 const btnKartuKredit = document.getElementById('btn-kartu-kredit');
 const btnPinjaman = document.getElementById('btn-pinjaman');
-const btnInvestasi = document.getElementById('btn-investasi');*/
+const btnInvestasi = document.getElementById('btn-investasi');
+
+function tampilkanNotifikasi(event, namaProduk) {
+    event.preventDefault(); // Mencegah aksi default dari tombol
+    alert(`Terima kasih telah mengajukan ${namaProduk}. Tim kami akan segera menghubungi Anda untuk proses selanjutnya.`);
+}
+
+btnTabungan.addEventListener('click', function(e)  {
+    tampilkanNotifikasi(e, 'Tabungan Safari');
+});
+
+btnKartuKredit.addEventListener('click', function(e)  {
+    tampilkanNotifikasi(e, 'Kartu Kredit Safari');
+});
+
+btnPinjaman.addEventListener('click', function(e)  {
+    tampilkanNotifikasi(e, 'Pinjaman Safari');
+});
+
+btnInvestasi.addEventListener('click', function(e)  {
+    tampilkanNotifikasi(e, 'Investasi Safari');
+});

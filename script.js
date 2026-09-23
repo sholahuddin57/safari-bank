8545140d17bf3e3d723cd77129a2526ea47564f8
sholahuddin57 <sholahuddin.nurfuady@gmail.com>
2026-09-23 21:04:40 +0700

    feat: menambahkan logika JS alert pada tombol tabungan safari

---
+++ script.js
@@ -0,0 +1,11 @@
+const btnTabungan = document.getElementById('btn-tabungan');
+    btnTabungan.addEventListener('click', function() {
+        event.preventDefault();
+        alert('Terimakasih! Pengajuan Tabungan Safari Anda sedang diproses dan akan segera dikonfirmasi oleh tim kami.');
+    });
+    
+
+/*
+const btnKartuKredit = document.getElementById('btn-kartu-kredit');
+const btnPinjaman = document.getElementById('btn-pinjaman');
+const btnInvestasi = document.getElementById('btn-investasi');*/

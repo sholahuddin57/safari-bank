6902d80a86b2e30152ab018adaa3f2acbd1f6cc2
sholahuddin57 <sholahuddin.nurfuady@gmail.com>
2026-09-23 23:44:01 +0700

    feat: setup koneksi database mysql

---
+++ koneksi.php
@@ -0,0 +1,19 @@
+<?php
+// 1. Menyiapkan kredensial (kunci) bawaan pabrik dari XAMPP
+$host = "localhost";
+$user = "root";      // Username default XAMPP adalah 'root'
+$pass = "";          // Password default XAMPP dibiarkan kosong
+$db   = "safari_bank"; // Nama database yang kamu buat di phpMyAdmin
+
+// 2. Membangun jembatan koneksi
+$koneksi = mysqli_connect($host, $user, $pass, $db);
+
+// 3. Control Structure (If-Else): Mengecek apakah jembatan berhasil dibangun
+if (!$koneksi) {
+    // Jika gagal (tanda seru berarti 'tidak/gagal'), hentikan proses dan tampilkan error
+    die("Koneksi database gagal: " . mysqli_connect_error());
+} else {
+    // Jika berhasil, tampilkan pesan sukses
+    echo "Koneksi ke pangkalan data Safari Bank sukses!";
+}
+?>
